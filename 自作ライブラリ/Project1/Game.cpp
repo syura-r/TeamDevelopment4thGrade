e6c934a58d65777417646c2f08c363cbd8c9fea6
SaitoKai <kai123saito@gmail.com>
2022-06-19 12:43:50 +0900

    サウンド読み込み問題なし

--- 自作ライブラリ/Project1/Game.cpp
+++ 自作ライブラリ/Project1/Game.cpp
@@ -127,6 +127,20 @@ void Game::RoadAsset()
 
 		//WAVファイルの読み込み
 		//Audio::LoadFile("test", "51e7ff6cc6b93897.wav");
+		Audio::LoadFile("BGM_Title", "BGM/Title_BGM.wav");
+		Audio::LoadFile("BGM_Play", "BGM/GamePlay_BGM.wav");
+		Audio::LoadFile("BGM_Result", "BGM/Result_BGM.wav");
+		Audio::LoadFile("SE_Decision", "SE/Allscene_SE_Decision.wav");
+		Audio::LoadFile("SE_Select", "SE/Allscene_SE_Select.wav");
+		Audio::LoadFile("SE_Collision", "SE/GamePlay_SE_Collision.wav");
+		Audio::LoadFile("SE_Dash", "SE/GamePlay_SE_Dash.wav");
+		Audio::LoadFile("SE_Fall", "SE/GamePlay_SE_Fall.wav");
+		Audio::LoadFile("SE_GetSaw", "SE/GamePlay_SE_Getsaw.wav");
+		Audio::LoadFile("SE_GetTriangle", "SE/GamePlay_SE_GetTriangle.wav");
+		Audio::LoadFile("SE_SawCutNow", "SE/GamePlay_SE_Sawcut_Now.wav");
+		Audio::LoadFile("SE_SawCutOut", "SE/GamePlay_SE_Sawcut_Out.wav");
+		Audio::LoadFile("SE_SteppingOn", "SE/GamePlay_SE_Steppingon.wav");
+		Audio::LoadFile("SE_TriangleLost", "SE/GamePlay_SE_Trianglelost.wav");
 		break;
 	case 5:
 		//FBXアニメーションの登録
