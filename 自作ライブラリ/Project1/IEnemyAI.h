0f8cb9fa1bb963580622e508b87d330f0459434b
Amnesio <74130581+Amnesio@users.noreply.github.com>
2022-07-08 14:10:24 +0900

    IEnemyAIの関数定義

--- 自作ライブラリ/Project1/IEnemyAI.h
+++ 自作ライブラリ/Project1/IEnemyAI.h
@@ -12,8 +12,17 @@ enum class EnemyAILabel
 class IEnemyAI
 {
 public:	
-	virtual void Update(StandardEnemy* arg_enemy, Vector3& arg_velocity, ActionStateLabel& arg_stateLabel) = 0;
-	virtual bool IsChangeMoveToCUt(StandardEnemy* arg_enemy) = 0;
+	virtual Vector3 KeepAwayFromGottenPieces(StandardEnemy* arg_enemy, const Vector3& arg_velocity) = 0;
+	virtual Vector3 KeepAwayFromFieldBorder(StandardEnemy* arg_enemy, const Vector3& arg_velocity) = 0;
+	virtual Vector3 AgainstFieldTilt(StandardEnemy* arg_enemy, const Vector3& arg_velocity) = 0;
+	virtual Vector3 ApproachEnergyItem(StandardEnemy* arg_enemy, const Vector3& arg_velocity) = 0;
+	virtual Vector3 ApproachCuttingActor(StandardEnemy* arg_enemy, const Vector3& arg_velocity) = 0;
+
+	virtual bool StartCutOnSafeTiming(StandardEnemy* arg_enemy) = 0;
+	virtual bool StartCutEnoughActivePieces(StandardEnemy* arg_enemy) = 0;
+	virtual bool StartCutIncludeBonus(StandardEnemy* arg_enemy) = 0;
+	virtual bool StartCutReachFever(StandardEnemy* arg_enemy) = 0;
+	virtual bool StartCutKillActorInFever(StandardEnemy* arg_enemy) = 0;
 
 	virtual EnemyAILabel GetLabel()const = 0;
 
